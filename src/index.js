0471bfd3dddbcf13f391f4af6d622b57775d9137
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-05-11 10:36:16 -0300

    Added more integration tests

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ class Todos {
   }
 
   complete(title) {
+    if (this.todos.length === 0) {
+      throw new Error("You have no TODOs stored. Why don't you add one first?");
+    }
+    
     this.todos.forEach((todo) => {
       if (todo.title === title) {
         todo.completed = true;
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,5 +5,26 @@ describe("Integration Test", () => {
   it("Should be able to add and complete TODOs", () => {
     let todos = new Todos();
     assert.strictEqual(todos.list().length, 0);
+
+    todos.add("run code");
+    assert.strictEqual(todos.list().length, 1);
+    assert.deepStrictEqual(todos.list(), [{title: "run code", completed: false}]);
+
+    todos.add("test everything");
+    assert.strictEqual(todos.list().length, 2);
+    assert.deepStrictEqual(todos.list(),
+      [
+        { title: "run code", completed: false },
+        { title: "test everything", completed: false }
+      ]
+    );
+
+    todos.complete("run code");
+    assert.deepStrictEqual(todos.list(),
+      [
+        { title: "run code", completed: true },
+        { title: "test everything", completed: false }
+      ]
+    );
   });
 });
