52ad90021b324f8abf396a4c612d368ef04cc36c
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-05-10 21:14:47 -0300

    Added functions in class Todos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ class Todos {
   constructor() {
     this.todos = [];
   }
+
+  list() {
+    return this.todos;
+  }
+
+  add(title) {
+    let todo = {
+      title: title,
+      completed: false
+    }
+
+    this.todos.push(todo);
+  }
+
+  complete(title) {
+    this.todos.forEach((todo) => {
+      if (todo.title === title) {
+        todo.completed = true;
+        return;
+      }
+    });
+  }
 }
 
 module.exports = Todos;
